2e72207ec95657f009618e87f858b85aa05583cc
Yuguang Tong <ygtong@berkeley.edu>
2019-07-19 13:20:42 -0700

    add comments to util.py and bimax.py

--- qtn/bimax.py
+++ qtn/bimax.py
@@ -5,15 +5,28 @@ from .util import (zp, zpd, zp2d, j0, f1, f2, fperp, timing)
 from .util import (boltzmann, emass, echarge, permittivity, cspeed)
 
 
+# These class provides methods to calculate
+#   electron noise
+#   proton noise
+#   shot noise
+#   transfer function (gain)
+# for a specified (long) dipole wire antenna
+#
+# The implementation uses expressions from the classical paper by N. Meyer-Vernet et al. (1989)
+#         https://agupubs.onlinelibrary.wiley.com/doi/abs/10.1029/JA094iA03p02405
+# The proton noise calculation follows Issautier et al. (1999)
+#         https://agupubs.onlinelibrary.wiley.com/doi/pdf/10.1029/1998JA900165
+
 class BiMax(object):
     def __init__(self, ant_len, ant_rad, base_cap):
         self.ant_len = ant_len
         self.ant_rad = ant_rad
-        self.al_ratio = ant_rad/ant_len
+        self.al_ratio = ant_rad / ant_len
         self.base_cap = base_cap
+        # Unit conversion constants to deliver end results in SI units
         self.z_unit = 8.313797e6
         self.v_unit = 1.62760e-15
-        
+
     @staticmethod
     def d_l(z, wc, n, t):
         """
@@ -24,40 +37,40 @@ class BiMax(object):
         t: T_h/T_c
     
         """
-        return 1 - (z/wc)**2 * (zpd(z) + n/t * zpd(z/mp.sqrt(t)))
-    
+        return 1 - (z / wc) ** 2 * (zpd(z) + n / t * zpd(z / mp.sqrt(t)))
+
     @staticmethod
     def dz_dl(z, wc, n, t):
         """
         partial derivative of dl w.r.t z
         
         """
-        zwc2 = (z/wc)**2
-        nt = n/t
+        zwc2 = (z / wc) ** 2
+        nt = n / t
         sqt = mp.sqrt(t)
-        zsqt = z/sqt
+        zsqt = z / sqt
         result = -2 * zwc2 / z * (zpd(z) + nt * zpd(zsqt))
-        result -= zwc2 * (zp2d(z) + nt/sqt * zp2d(zsqt))
+        result -= zwc2 * (zp2d(z) + nt / sqt * zp2d(zsqt))
         return result
 
     @staticmethod
     def dz2_dl(z, wc, n, t):
         """
         second order P.D. \frac{\partial^2 dl}{\partial z^2}
-        The expansion is done by Mathematica. 
+        The expansion was done by Mathematica.
         """
-        z2 = z*z
+        z2 = z * z
         z4 = z2 * z2
         t2 = t * t
         t3 = t2 * t
         tsq = mp.sqrt(t)
         term_1 = 1 - 6 * z2 + 2 * z4
-        term_2 = (n/t3) * (t2 - 6 * t * z2 + 2 * z4)
+        term_2 = (n / t3) * (t2 - 6 * t * z2 + 2 * z4)
         term_3 = z * (3 - 7 * z2 + 2 * z4) * zp(z)
-        term_4 = n * z / t3 / tsq * (3 * t2 - 7 * t * z2 + 2 * z4) * zp(z/tsq)
-        
-        return 4 * (term_1 + term_2 + term_3 + term_4) / wc**2
-        
+        term_4 = n * z / t3 / tsq * (3 * t2 - 7 * t * z2 + 2 * z4) * zp(z / tsq)
+
+        return 4 * (term_1 + term_2 + term_3 + term_4) / wc ** 2
+
     @staticmethod
     def long_interval(w, n, t):
         """
@@ -66,13 +79,13 @@ class BiMax(object):
         d_l(root, w, n, t) = 0.
         
         """
-        if w < mp.sqrt(1+n):
+        if w < mp.sqrt(1 + n):
             return [0, mp.inf]
         if n == 0:
             guesses = [4, 6, 8, 10]
         else:
             guesses = [4, 6, 8, 10, 12, 14]
-        int_range = [0, mp.inf]    
+        int_range = [0, mp.inf]
         for guess in guesses:
             try:
                 root = mp.findroot(lambda z: BiMax.d_l(z, w, n, t).real, guess)
@@ -94,11 +107,11 @@ class BiMax(object):
         a: a/l is the ratio of antenna radius and monopole length
         
         """
-        kl = wc*l/mp.sqrt(2)/z
+        kl = wc * l / mp.sqrt(2) / z
         ka = kl * self.al_ratio
-        num = f1(kl) * j0(ka)**2
-        denom = z**2 * BiMax.d_l(z, wc, n, t)
-        return num/denom
+        num = f1(kl) * j0(ka) ** 2
+        denom = z ** 2 * BiMax.d_l(z, wc, n, t)
+        return num / denom
 
     def za_l(self, wc, l, n, t, tc):
         """
@@ -108,18 +121,18 @@ class BiMax(object):
 
         """
         limits = self.long_interval(wc, n, t)
-        #print(limits)
+        # print(limits)
         result = mp.quad(lambda z: self.za_l_integrand(z, wc, l, n, t), limits)
-        return result * self.z_unit * mp.mpc(0, 1)/ mp.sqrt(tc)
+        return result * self.z_unit * mp.mpc(0, 1) / mp.sqrt(tc)
 
     def bimax_integrand(self, z, wc, l, n, t):
         """
         Integrand of electron-noise integral.
         
         """
-        return f1(wc*l/z/mp.sqrt(2)) * z * \
-            (mp.exp(-z**2) + n/mp.sqrt(t)*mp.exp(-z**2 / t)) / \
-            (mp.fabs(BiMax.d_l(z, wc, n, t))**2 * wc**2)
+        return f1(wc * l / z / mp.sqrt(2)) * z * \
+               (mp.exp(-z ** 2) + n / mp.sqrt(t) * mp.exp(-z ** 2 / t)) / \
+               (mp.fabs(BiMax.d_l(z, wc, n, t)) ** 2 * wc ** 2)
 
     def bimax(self, wrel, l, n, t, tc):
         """
@@ -127,9 +140,9 @@ class BiMax(object):
         w: f/f_p, where f_p is the total plasma frequency.
         
         """
-        wc = wrel * mp.sqrt(1+n)
+        wc = wrel * mp.sqrt(1 + n)
         limits = self.long_interval(wc, n, t)
-        #print(limits)
+        # print(limits)
         result = mp.quad(lambda z: self.bimax_integrand(z, wc, l, n, t), limits)
         return result * self.v_unit * mp.sqrt(tc)
 
@@ -139,10 +152,10 @@ class BiMax(object):
         The antenna monopole length 45m is hardwired in.
         
         """
-        ldc = self.ant_len/l
-        nc = permittivity * boltzmann * tc/ ldc**2 / echarge**2
-        wpc = mp.sqrt( nc * echarge**2 / emass / permittivity)
-        return mp.mpc(0, 1/(wc * wpc * self.base_cap))
+        ldc = self.ant_len / l
+        nc = permittivity * boltzmann * tc / ldc ** 2 / echarge ** 2
+        wpc = mp.sqrt(nc * echarge ** 2 / emass / permittivity)
+        return mp.mpc(0, 1 / (wc * wpc * self.base_cap))
 
     def gamma_shot(self, wrel, l, n, t, tc):
         """
@@ -155,23 +168,23 @@ class BiMax(object):
             mp.mp.dps = 80
         else:
             mp.mp.dps = 40
-        wc = wrel * mp.sqrt(1+n)
+        wc = wrel * mp.sqrt(1 + n)
         za = self.za_l(wc, l, n, t, tc)
         mp.mp.dps = 15
         zr = self.zr_mp(wc, l, tc)
 
         # below calculating shot noise:
-        ldc = self.ant_len/l
-        nc  =permittivity * boltzmann * tc/ ldc**2 / echarge**2
-        vtc = np.sqrt(2 * boltzmann * tc/ emass)
+        ldc = self.ant_len / l
+        nc = permittivity * boltzmann * tc / ldc ** 2 / echarge ** 2
+        vtc = np.sqrt(2 * boltzmann * tc / emass)
         ne = nc * vtc * (1 + n * mp.sqrt(t)) * 2 * np.pi * self.ant_rad * self.ant_len / np.sqrt(4 * np.pi)
         ###################
         ## a: coefficient in shot noise. see Issautier et al. 1999
         ###################
-        a = 1 + echarge * 3.6 / boltzmann/tc
-        shot_noise = 2 * a * echarge**2 * mp.fabs(za)**2 * ne        
-        return [mp.fabs((zr+za)/zr)**2, shot_noise]
- 
+        a = 1 + echarge * 3.6 / boltzmann / tc
+        shot_noise = 2 * a * echarge ** 2 * mp.fabs(za) ** 2 * ne
+        return [mp.fabs((zr + za) / zr) ** 2, shot_noise]
+
     def proton(self, f, ne, n, t, tp, tc, vsw):
         """
         proton noise.
@@ -184,20 +197,20 @@ class BiMax(object):
 
         """
         ne = ne * 1e6
-        tp = tp * echarge/boltzmann
-        tc = tc * echarge/boltzmann
-        w_p = np.sqrt(echarge**2 * ne/emass/permittivity)
-        te = (tc + tc * t * n)/(1+n)
-        tg = tc * (1 + n)/(1 + n/t)
-        ld = np.sqrt(permittivity * boltzmann * tg/ne/ echarge**2)
-        lrel = self.ant_len/ld
-        vte = np.sqrt(2 * boltzmann * te/ emass)
-        omega = f * 2 * np.pi * ld/vsw
-        tep = tg/tp
-        M = vsw/vte
-        integrand = lambda y: y * fperp(y * lrel)/ (y**2 + 1 + omega**2) / (y**2 + 1 + omega**2 + tep)
-        integral = scint.quad(integrand, 0, np.inf, epsrel = 1.e-4) 
-        return mp.sqrt(2*emass*boltzmann*tg)/(4*mp.pi*permittivity* M) *  integral[0]
+        tp = tp * echarge / boltzmann
+        tc = tc * echarge / boltzmann
+        w_p = np.sqrt(echarge ** 2 * ne / emass / permittivity)
+        te = (tc + tc * t * n) / (1 + n)
+        tg = tc * (1 + n) / (1 + n / t)
+        ld = np.sqrt(permittivity * boltzmann * tg / ne / echarge ** 2)
+        lrel = self.ant_len / ld
+        vte = np.sqrt(2 * boltzmann * te / emass)
+        omega = f * 2 * np.pi * ld / vsw
+        tep = tg / tp
+        M = vsw / vte
+        integrand = lambda y: y * fperp(y * lrel) / (y ** 2 + 1 + omega ** 2) / (y ** 2 + 1 + omega ** 2 + tep)
+        integral = scint.quad(integrand, 0, np.inf, epsrel=1.e-4)
+        return mp.sqrt(2 * emass * boltzmann * tg) / (4 * mp.pi * permittivity * M) * integral[0]
 
     def electron_noise(self, f, ne, n, t, tp, tc, vsw):
         """
@@ -206,12 +219,12 @@ class BiMax(object):
 
         """
         ne *= 1e6
-        nc = ne/(1+n)
-        tc *= echarge/ boltzmann
-        ldc = np.sqrt(permittivity * boltzmann * tc/nc/echarge**2)
-        lc = self.ant_len/ldc
-        w_p = np.sqrt(echarge**2 * ne/emass/permittivity)
-        wrel = f * 2 * mp.pi/w_p
+        nc = ne / (1 + n)
+        tc *= echarge / boltzmann
+        ldc = np.sqrt(permittivity * boltzmann * tc / nc / echarge ** 2)
+        lc = self.ant_len / ldc
+        w_p = np.sqrt(echarge ** 2 * ne / emass / permittivity)
+        wrel = f * 2 * mp.pi / w_p
         return self.bimax(wrel, lc, n, t, tc)
 
     def gain_shot(self, f, ne, n, t, tp, tc, vsw):
@@ -220,10 +233,10 @@ class BiMax(object):
         
         """
         ne *= 1e6
-        nc = ne/(1+n)
-        tc *= echarge/ boltzmann
-        ldc = np.sqrt(permittivity * boltzmann * tc/nc/echarge**2)
-        lc = self.ant_len/ldc
-        w_p = np.sqrt(echarge**2 * ne/emass/permittivity)
-        wrel = f * 2 * mp.pi/w_p
+        nc = ne / (1 + n)
+        tc *= echarge / boltzmann
+        ldc = np.sqrt(permittivity * boltzmann * tc / nc / echarge ** 2)
+        lc = self.ant_len / ldc
+        w_p = np.sqrt(echarge ** 2 * ne / emass / permittivity)
+        wrel = f * 2 * mp.pi / w_p
         return self.gamma_shot(wrel, lc, n, t, tc)
--- qtn/util.py
+++ qtn/util.py
@@ -7,25 +7,30 @@ import scipy.special
 from scipy.special import j1, itj0y0, sici
 
 # fundamental constants
+# (ygtong): Retrospectively, these constants could have been imported from scipy.constants
+# but refactor could be painful ...
 boltzmann = 1.3806488e-23  # J/K
-emass = 9.10938291e-31     # kg
+emass = 9.10938291e-31  # kg
 pmass = 1.67262178e-27
-echarge = 1.60217657e-19   # C
+echarge = 1.60217657e-19  # C
 permittivity = 8.854187817e-12  # F/m
-cspeed = 299792458         # m/s
+cspeed = 299792458  # m/s
 
 
 def timing(f):
     """
     time decorator
     """
+
     def wrap(*args):
         time1 = time.time()
         ret = f(*args)
         time2 = time.time()
-        print('{0} function took {1:.3f} ms'.format(f.__name__, (time2-time1)*1000.0))
+        print('{0} function took {1:.3f} ms'.format(f.__name__, (time2 - time1) * 1000.0))
         return ret
+
     return wrap
+
 
 def ldeb(ne, te):
     """ 
@@ -36,8 +41,9 @@ def ldeb(ne, te):
     ~ 69.01 * sqrt(te/ne)
 
     """
-    
-    return mp.sqrt(permittivity * boltzmann/ echarge**2) * mp.sqrt(te/ne)
+
+    return mp.sqrt(permittivity * boltzmann / echarge ** 2) * mp.sqrt(te / ne)
+
 
 def fp(ne):
     """ 
@@ -45,27 +51,34 @@ def fp(ne):
     ~ 8.98 * sqrt(ne)
 
     """
-    
-    return mp.sqrt(echarge**2/emass/permittivity)/2/mp.pi * mp.sqrt(ne)
 
+    return mp.sqrt(echarge ** 2 / emass / permittivity) / 2 / mp.pi * mp.sqrt(ne)
+
+
+# Two arrays that appear in 8-pole Pade approximation to plasma dispersion relation function.
+# See the technical report accompany the Fortran code WHAMP, Ronnmark (1985)
+#   https://link.springer.com/article/10.1007/BF00214996
+# Pade approximation is fast because it doesn't involve any special functions. But the error
+#   might be large outside of certain area in the complex plane.
 c_arr = np.array([
-2.237687789201900 - 1.625940856173727j,
--2.237687789201900 - 1.625940856173727j,
-1.465234126106004 - 1.789620129162444j,
--1.465234126106004 - 1.789620129162444j,
-0.8392539817232638 - 1.891995045765206j,
--0.8392539817232638 - 1.891995045765206j,
-0.2739362226285564 - 1.941786875844713j,
--0.2739362226285564 -1.941786875844713j])
+    2.237687789201900 - 1.625940856173727j,
+    -2.237687789201900 - 1.625940856173727j,
+    1.465234126106004 - 1.789620129162444j,
+    -1.465234126106004 - 1.789620129162444j,
+    0.8392539817232638 - 1.891995045765206j,
+    -0.8392539817232638 - 1.891995045765206j,
+    0.2739362226285564 - 1.941786875844713j,
+    -0.2739362226285564 - 1.941786875844713j])
 
 b_arr = np.array([-0.01734012457471826 - 0.04630639291680322j,
--0.01734012457471826 + 0.04630639291680322j,
--0.7399169923225014 + 0.8395179978099844j,
--0.7399169923225014 - 0.8395179978099844j,
-5.840628642184073 + 0.9536009057643667j,
-5.840628642184073 - 0.9536009057643667j,
--5.583371525286853 - 11.20854319126599j,
--5.583371525286853 + 11.20854319126599j])
+                  -0.01734012457471826 + 0.04630639291680322j,
+                  -0.7399169923225014 + 0.8395179978099844j,
+                  -0.7399169923225014 - 0.8395179978099844j,
+                  5.840628642184073 + 0.9536009057643667j,
+                  5.840628642184073 - 0.9536009057643667j,
+                  -5.583371525286853 - 11.20854319126599j,
+                  -5.583371525286853 + 11.20854319126599j])
+
 
 def zp_pade(z):
     """
@@ -76,7 +89,8 @@ def zp_pade(z):
     
     Return the value of Zp(z) using Pade approximations.
     """
-    return np.sum(b_arr/(z-c_arr))
+    return np.sum(b_arr / (z - c_arr))
+
 
 def zp_sp(z):
     """
@@ -87,8 +101,9 @@ def zp_sp(z):
     
     Return the value of Zp(z)
     """
-    return -2. * scipy.special.dawsn(z) + 1.j * np.sqrt(np.pi) * np.exp(- z **2)
-    
+    return -2. * scipy.special.dawsn(z) + 1.j * np.sqrt(np.pi) * np.exp(- z ** 2)
+
+
 def zpd_sp(x):
     """
     Derivative of the plasma dispersion function
@@ -96,13 +111,15 @@ def zpd_sp(x):
     """
     return -2 * (1 + x * zp_sp(x))
 
+
 def zp(x):
     """
     plasma dispersion function                                
     using complementary error function in mpmath library.                       
                                                                                 
     """
-    return -mp.sqrt(mp.pi) * mp.exp(-x**2) * mp.erfi(x) + mpc(0, 1) * mp.sqrt(mp.pi) * mp.exp(-x**2)
+    return -mp.sqrt(mp.pi) * mp.exp(-x ** 2) * mp.erfi(x) + mpc(0, 1) * mp.sqrt(mp.pi) * mp.exp(-x ** 2)
+
 
 def zpd(x):
     """
@@ -111,11 +128,13 @@ def zpd(x):
     """
     return -mpf(2) * (mpf(1) + x * zp(x))
 
+
 def zp2d(x):
     """
     second derivative of plasma dispersion function.
     """
-    return 4 * x - 2 * zp(x) + 4 * x**2 * zp(x)
+    return 4 * x - 2 * zp(x) + 4 * x ** 2 * zp(x)
+
 
 def j0(x):
     """zeroth order bessel function, the argument may be of "mpf" type.
@@ -124,65 +143,80 @@ def j0(x):
     """
     return scipy.special.j0(float(str(x)))
 
+
 def eta(tc):
     """
     eta = v_{tc}/c, where v_tc is the thermal speed
     of core electrons whose temperature is tc (Kelvin)
     
     """
-    return mp.sqrt(2 * boltzmann * tc / emass)/ cspeed
+    return mp.sqrt(2 * boltzmann * tc / emass) / cspeed
+
 
 def f1(x):
     """
     An angular integral that appears in electron noise calculation.
+    See N. Meyer-Vernet et al. (1989)
+        https://agupubs.onlinelibrary.wiley.com/doi/abs/10.1029/JA094iA03p02405
     
     """
-    term1 = x * (mp.si(x) - 0.5 * mp.si(2*x))
-    term2 = -2 * mp.sin(0.5 * x)**4
-    return (term1 + term2)/x**2
+    term1 = x * (mp.si(x) - 0.5 * mp.si(2 * x))
+    term2 = -2 * mp.sin(0.5 * x) ** 4
+    return (term1 + term2) / x ** 2
+
 
 def f2(x):
     """
     Another angular integral in electron noise calculation.
-    
+    See N. Meyer-Vernet et al. (1989)
+        https://agupubs.onlinelibrary.wiley.com/doi/abs/10.1029/JA094iA03p02405
+
     """
-    term1 = 2*x**3 * (2*mp.si(2*x)-mp.si(x))
-    term2 = 2*x**2 * (mp.cos(2*x) - mp.cos(x))
-    term3 = x * (mp.sin(2*x) - 2*mp.sin(x)) - (mp.cos(2*x) - 4* mp.cos(x)  + 3)
-    return (term1 + term2 + term3)/(12 * x**2)
+    term1 = 2 * x ** 3 * (2 * mp.si(2 * x) - mp.si(x))
+    term2 = 2 * x ** 2 * (mp.cos(2 * x) - mp.cos(x))
+    term3 = x * (mp.sin(2 * x) - 2 * mp.sin(x)) - (mp.cos(2 * x) - 4 * mp.cos(x) + 3)
+    return (term1 + term2 + term3) / (12 * x ** 2)
+
 
 def f1_sp(x):
     """
     An angular integral that appears in electron noise calculation.
+    Definition is the same as f1 above, but scipy functions are used for speed.
     
     """
-    term1 = x * (sici(x)[0] - 0.5 * sici(2*x)[0])
-    term2 = -2 * np.sin(0.5 * x)**4
-    return (term1 + term2)/x**2
+    term1 = x * (sici(x)[0] - 0.5 * sici(2 * x)[0])
+    term2 = -2 * np.sin(0.5 * x) ** 4
+    return (term1 + term2) / x ** 2
+
 
 def f2_sp(x):
     """
     Another angular integral in electron noise calculation.
-    
+    Definition is the same as f2 above, but scipy functions are used for speed.
     """
-    term1 = 2*x**3 * (2*np.si(2*x)-np.si(x))
-    term2 = 2*x**2 * (np.cos(2*x) - p.cos(x))
-    term3 = x * (np.sin(2*x) - 2*np.sin(x)) - (np.cos(2*x) - 4* np.cos(x)  + 3)
-    return (term1 + term2 + term3)/(12 * x**2)
+    term1 = 2 * x ** 3 * (2 * np.si(2 * x) - np.si(x))
+    term2 = 2 * x ** 2 * (np.cos(2 * x) - p.cos(x))
+    term3 = x * (np.sin(2 * x) - 2 * np.sin(x)) - (np.cos(2 * x) - 4 * np.cos(x) + 3)
+    return (term1 + term2 + term3) / (12 * x ** 2)
+
 
 def fperp(x):
     """
     result of angular integration in proton noise integration.
+    See Issautier et al. (1999)
+        https://agupubs.onlinelibrary.wiley.com/doi/pdf/10.1029/1998JA900165
     
     """
-    return 8./x * (2*itj0y0(x)[0] - itj0y0(2*x)[0] + j1(2*x) - 2*j1(x))
+    return 8. / x * (2 * itj0y0(x)[0] - itj0y0(2 * x)[0] + j1(2 * x) - 2 * j1(x))
+
 
 def zk(z, k):
     """
     modified dispersion function for Kappa distribution.
-    (Mace and Hellberg, 1995)
+    See Mace and Hellberg (1995)
+        https://aip.scitation.org/doi/10.1063/1.871296
     
     """
     i = mp.mpc(0, 1)
-    coeff = i * (k + 0.5) * (k-0.5) / (mp.sqrt(k**3) * (k+1))
-    return coeff * hyp2f1(1, 2*k+2, k+2, (1-z/(i * mp.sqrt(k)))/2)
+    coeff = i * (k + 0.5) * (k - 0.5) / (mp.sqrt(k ** 3) * (k + 1))
+    return coeff * hyp2f1(1, 2 * k + 2, k + 2, (1 - z / (i * mp.sqrt(k))) / 2)
